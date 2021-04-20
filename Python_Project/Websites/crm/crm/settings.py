d7a3ce3e924c3e83a1668e4ca501c92b1702eb00
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2021-04-20 12:10:03 +0700

    Perubahan baru

--- Python_Project/Websites/crm/crm/settings.py
+++ Python_Project/Websites/crm/crm/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nazhimmaulanamuhammad@gmail.com'
-EMAIL_HOST_PASSWORD = 'Brolyandgoku0.2'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
